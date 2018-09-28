397d7faf81a0030c69143b7b8df630d207b8a5ef
20161104612 <643024265@qq.com>
2018-09-28 16:05:57 +0800

    1+2
    1+2

--- hello1/ViewController.swift
+++ hello1/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var test2: UITextField!
+    
+    @IBOutlet weak var test: UITextField!
+    
+    @IBOutlet weak var test1: UITextField!
+    
+    @IBOutlet weak var test3: UITextField!
+    
+    @IBAction func bu(_ sender: Any) {
+        test2.text = " hello world! "
+        test.text = "\(Int(test1.text!)! + Int(test3.text!)!)"
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
